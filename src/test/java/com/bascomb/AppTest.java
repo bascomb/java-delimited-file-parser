b6fd61937ac181ba25c5b8deb9e02748a333cc8f
Bascomb Arnette <bascomb.professional@gmail.com>
2019-08-27 17:49:24 -0700

    New Test. Apach Commons FileUtils. Print all Lines of file

--- src/test/java/com/bascomb/AppTest.java
+++ src/test/java/com/bascomb/AppTest.java
@@ -2,26 +2,31 @@ package com.bascomb;
 
 import static org.junit.Assert.assertTrue;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.LineIterator;
 import org.junit.Test;
 
 import java.io.File;
-import java.net.URL;
+import java.io.IOException;
+import java.nio.file.Path;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
-    URL url = this.getClass().getResource("/test-tab-delimited-file.tab");
-    File testTabFile = new File(url.getFile());
-
+    File testFile = new File("src/main/resources/test-tab-delimited-file.tab");
     /**
      * test-tab-delimited-file.tab
      */
     @Test
-    public void testTabDelimitedFile()
-    {
-        System.out.println(testTabFile.toPath());
+    public void testTabDelimitedFile() throws IOException {
+        System.out.println(testFile.toPath());
+        LineIterator it = FileUtils.lineIterator(testFile);
+        while(it.hasNext()) {
+            String line = it.nextLine();
+            System.out.println(line);
+        }
         assertTrue(true);
     }
 }

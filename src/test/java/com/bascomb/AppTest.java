bf93fb4a0e39e187f14857f9f994743a981ab29b
Bascomb Arnette <bascomb.professional@gmail.com>
2019-08-27 17:55:37 -0700

    Test update. Read list of strings. compare to expected list.

--- src/test/java/com/bascomb/AppTest.java
+++ src/test/java/com/bascomb/AppTest.java
@@ -1,5 +1,6 @@
 package com.bascomb;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.apache.commons.io.FileUtils;
@@ -9,24 +10,27 @@ import org.junit.Test;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
-    File testFile = new File("src/main/resources/test-tab-delimited-file.tab");
+    File file = new File("src/main/resources/test-tab-delimited-file.tab");
     /**
      * test-tab-delimited-file.tab
      */
     @Test
     public void testTabDelimitedFile() throws IOException {
-        System.out.println(testFile.toPath());
-        LineIterator it = FileUtils.lineIterator(testFile);
-        while(it.hasNext()) {
-            String line = it.nextLine();
-            System.out.println(line);
+        System.out.println(file.toPath());
+        List<String> lines = FileUtils.readLines(file, "UTF-8");
+        List<String> expectedLines = new ArrayList<String>();
+        expectedLines.add("This    is  a   test");
+        expectedLines.add("This    is  also    a   test");
+        for(int i = 0; i < lines.size(); i++) {
+            assertEquals(lines.get(i), expectedLines.get(i));
         }
-        assertTrue(true);
     }
 }

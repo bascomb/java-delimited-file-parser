d2b5263f82e82b70a387fe2ac6fa24994a66bafe
Bascomb Arnette <bascomb.professional@gmail.com>
2019-08-27 17:07:00 -0700

    First Unit Test. Test file placed in resources. App prints file name provided in args

--- src/test/java/com/bascomb/AppTest.java
+++ src/test/java/com/bascomb/AppTest.java
@@ -4,17 +4,24 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import java.io.File;
+import java.net.URL;
+
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
+    URL url = this.getClass().getResource("/test-tab-delimited-file.tab");
+    File testTabFile = new File(url.getFile());
+
     /**
-     * Rigorous Test :-)
+     * test-tab-delimited-file.tab
      */
     @Test
-    public void shouldAnswerWithTrue()
+    public void testTabDelimitedFile()
     {
-        assertTrue( true );
+        System.out.println(testTabFile.toPath());
+        assertTrue(true);
     }
 }

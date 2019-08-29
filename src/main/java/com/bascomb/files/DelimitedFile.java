95370d2e8852216c32671143d63553babff890de
bascomb <bascomb.professional@gmail.com>
2019-08-29 14:01:59 -0700

    Attempting File Factory. Compile Error

--- src/main/java/com/bascomb/files/DelimitedFile.java
+++ src/main/java/com/bascomb/files/DelimitedFile.java
@@ -3,13 +3,14 @@ package com.bascomb.files;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.LineIterator;
 import org.apache.commons.text.StringTokenizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class    DelimitedFile extends Files{
-
-
 
     public DelimitedFile(String arg) {
         super(arg);

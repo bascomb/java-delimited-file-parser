82624c6db75752f1f72c04e913d081d180d899dd
Bascomb Arnette <bascomb.professional@gmail.com>
2019-08-29 20:48:27 -0700

    Revert "Attempting File Factory. Compile Error"
    This reverts commit 95370d2e8852216c32671143d63553babff890de.

--- src/main/java/com/bascomb/App.java
+++ src/main/java/com/bascomb/App.java
@@ -1,19 +1,16 @@
 package com.bascomb;
 
 import com.bascomb.files.Files;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Hello world!
  *
  */
-@SpringBootApplication
 public class App 
 {
     public static void main( String[] args )
     {
-        SpringApplication.run(App.class, args);
+
         System.out.println( "Processing File - " + args[0] );
     }
 }
